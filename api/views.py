797c30e7a4ff6aeca68e58cdda433d873034b271
kautukkundan <kautukkundan@gmail.com>
2020-03-09 01:29:49 +0530

    changes

--- api/views.py
+++ api/views.py
@@ -19,29 +19,37 @@ import string
 
 from core.models import Url
 
+
 class UserCreateAPIView(generics.CreateAPIView):
     queryset           = User.objects.all()
     serializer_class   = UserSerializer
     permission_classes = (AllowAny,)
 
-    def create(self, request, *args, **kwargs): 
+    def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         token, created = Token.objects.get_or_create(user=serializer.instance)
-        return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            {"token": token.key, "user": serializer.data},
+            status=status.HTTP_201_CREATED,
+            headers=headers,
+        )
+
 
 class UrlShortenerApiViewSet(ModelViewSet):
-    serializer_class       = UrlListSerializer
+    serializer_class = UrlListSerializer
 
     authentication_classes = (TokenAuthentication, SessionAuthentication)
-    permission_classes     = (permissions.IsAuthenticatedOrReadOnly, IsOwner,)
+    permission_classes     = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwner,
+    )
 
     def get_queryset(self):
         user = self.request.user
         return Url.objects.all().filter(owner=user)
 
-    
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
